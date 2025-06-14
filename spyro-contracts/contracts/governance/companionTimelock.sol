3490dc648d8c337aa3063b5d1a089d296c59ee67
Dexter <46444849+DexterZero@users.noreply.github.com>
2025-06-14 08:15:51 -0400

    Update companionTimelock.sol

--- spyro-contracts/contracts/governance/companionTimelock.sol
+++ spyro-contracts/contracts/governance/companionTimelock.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+import "@openzeppelin/contracts/governance/TimelockController.sol";
 
+contract SpyroTimelockController is TimelockController {
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors
+    )
+        TimelockController(minDelay, proposers, executors)
+    {}
+}
